63238dc9d69bacbc94e6387f5fe742b1d0bd18ca
joakimad <joakim.adolfsen@gmail.com>
2019-11-13 12:54:15 +0100

    Added read functions

--- src/Øving12/UnZipZapIt.java
+++ src/Øving12/UnZipZapIt.java
@@ -1,4 +1,28 @@
 package Øving12;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class UnZipZapIt {
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+    private static String readFile(String filename) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(filename));
+
+        StringBuilder content = new StringBuilder();
+        String line;
+
+        while ((line = reader.readLine()) != null) {
+            content.append(line);
+            content.append(System.lineSeparator());
+        }
+        return content.toString();
+    }
 }
+
--- src/Øving12/ZipZapIt.java
+++ src/Øving12/ZipZapIt.java
@@ -1,4 +1,28 @@
 package Øving12;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class ZipZapIt {
+
+    public static void main(String[] args) {
+
+        //READ FILE AND COMPRESS IT
+
+    }
+
+
+    private static String readFile(String filename) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(filename));
+
+        StringBuilder content = new StringBuilder();
+        String line;
+
+        while ((line = reader.readLine()) != null) {
+            content.append(line);
+            content.append(System.lineSeparator());
+        }
+        return content.toString();
+    }
 }
